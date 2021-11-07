e51b6cdf9495bb17e953904f06b58ff1fb6efd6a
Enver <enverdavid01@gmail.com>
2021-11-07 13:32:32 -0500

    Se culminó los ejercicios con filesytem

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 //Imports de los módulos
 //fs y path
+const fs = require("fs/promises");
 
-const getUsers = async() => {
-    //Leer el archivo
+const getUsers = async () => {
+  //Leer el archivo
 
-    //Regresar el arreglo de usuarios como objeto literal Javascript (sin notación JSON)
+  try {
+    const data = await fs.readFile("users.json", { encoding: "utf-8" });
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(err);
+  }
+
+  //Regresar el arreglo de usuarios como objeto literal Javascript (sin notación JSON)
 };
 
+
 const addUser = async (userObj) => {
-    //leer el archivo 
+    
+    try {
+      //leer el archivo
+    const data = await fs.readFile("users.json", { encoding: "utf-8" });
 
     //convertir el contenido en formato JSON en un objeto JS
+     const arreglo = JSON.parse(data);
 
-    //agregar el usuario en el arreglo
+     //agregar el usuario en el arreglo
+    arreglo.push(userObj)
 
     //sobreescribir el arreglo en el archivo
+    fs.writeFile("users.json", JSON.stringify(arreglo));
 
     //si el proceso se realizó correctamente tendrás que regresar el objeto de usuario
     //que acabas de agregar en el arreglo
+    return userObj;
+
+  } catch (error) {
+    console.log(err);
+  }
+
 };
 
 const clearUsers = async () => {
-    //Vaciar el arreglo y sobreescribir el archivo
-    
-    //Si el proceso se realizó correctamente tendrás que regresar true
-}
+  //Vaciar el arreglo y sobreescribir el archivo
+  try {
+    fs.writeFile("users.json", '[]');
+  } catch (error) {
+      console.log(error)
+  }
+  //Si el proceso se realizó correctamente tendrás que regresar true
+  return true
+};
 
 module.exports = {
-    getUsers,
-    addUser,
-    clearUsers,
+  getUsers,
+  addUser,
+  clearUsers,
 };
